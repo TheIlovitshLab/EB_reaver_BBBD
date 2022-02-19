e6564e3210f18c5c425a0dfc4ed7527666e73fca
sharonkatz510 <sharonkatz510@gmail.com>
2022-02-19 18:22:44 +0200

    completely revised how the function works. now it takes only a stripe at each distance

--- calc_eb_ext_upto_n_px.m
+++ calc_eb_ext_upto_n_px.m
@@ -2,7 +2,7 @@ function eb_ext_in_segments = calc_eb_ext_upto_n_px(rcind_seg_cell,all_seg_rads,
 %{
     Function to calculate EB extravasation around deifferent vessel segments
     according to previous vessel segmentation. return statistics for all
-    sements in 10 different extravasation distances from 1 to n pixels
+    sements in 10 different extravasation distances in rnage (0,n_px]
     Input arguements:
         rcind_seg_cell = cell array with every cell containing [row,col]
             coordinates of a vessel segment
@@ -15,31 +15,35 @@ function eb_ext_in_segments = calc_eb_ext_upto_n_px(rcind_seg_cell,all_seg_rads,
             the i,j element contains the median pixel value of a (j/10)*n_px 
             neighborhood around the i-th segment in the EB image
 %}
-eb_ext_in_segments = zeros(length(rcind_seg_cell),10); % Create a placeholder
+n_seg = length(rcind_seg_cell); % total nu,ber of segments
+eb_ext_in_segments = zeros(n_seg,10); % Create a placeholder
+perivasc_dist = round(n_px/10);
 
 
-for i=1:size(rcind_seg_cell,1)  % loop through all segments
-    se_wire = strel('disk',ceil(all_seg_rads(i)+ n_px),0); % Create a specific structure element for wire dilation 
+
+for i=1:n_seg  % loop through all segments
     lind_seg = sub2ind(size(bw_vessels), rcind_seg_cell{i}(:,1),rcind_seg_cell{i}(:,2));    % get all wire-frame elements of the segment (represent the middle-line)
     single_seg_bw = false(size(redIm));
     single_seg_bw(lind_seg) = 1;    % set only the segment wireframe to True
-    dilated_n_wire = imdilate(single_seg_bw,se_wire); % Dilate the wire
+    dilated_n_wire = zeros(size(redIm,1),size(redIm,2),11);
+    dilated_n_wire(:,:,1) = single_seg_bw;
     for j = 1:10
-        se_vessel = strel('disk',ceil((j/10)*(n_px/2)),0); % Create a universal structure element for vessel dilation
-        vessel_dilated = imdilate(bw_vessels,se_vessel); % dilate the vessel surrounding by n_px
-        perivasc = vessel_dilated - bw_vessels;
-        single_vessel_perivasc_mask = dilated_n_wire & perivasc;
-        single_vessel_mask = bw_vessels & dilated_n_wire; % Area within the vessel (inside the specific segment)
+        se_wire = strel('disk',ceil(all_seg_rads(i)+ perivasc_dist),8); % Create a specific structure element for wire dilation 
+        dilated_n_wire(:,:,j+1) = blockdilate(dilated_n_wire(:,:,j),se_wire);        
+        single_vessel_mask = bw_vessels & dilated_n_wire(:,:,2); % Area within the vessel (inside the specific segment)
+        single_vessel_perivasc_mask = ...
+            (dilated_n_wire(:,:,j+1) - dilated_n_wire(:,:,j));
+        single_vessel_perivasc_mask = ...
+            logical((single_vessel_perivasc_mask | bw_vessels) - bw_vessels);
         eb_ext_in_segments(i,j) = ...
             double(median(redIm(single_vessel_perivasc_mask),'all'))/...
             double(median(redIm(single_vessel_mask),'all'));
 %       %% Visualization if needed for debugging and n_px optimzation
-%       extra_vessel_red = redIm.*uint16(~bw_vessels); % remove vessles from red channel
-%       k = cat(3,extra_vessel_red,2^14.*uint16(single_vessel_perivasc_mask),...
-%           2^14.*uint16(vessel_dilated-bw_vessels));
-%       k = k + 2^16*uint16(repmat(single_seg_bw,1,1,3));
-%       imshow(k); 
-%       pause(0.1);
+%         extra_vessel_red = redIm.*uint16(~bw_vessels); % remove vessles from red channel
+%         k = cat(3,extra_vessel_red,2^14.*uint16(single_vessel_perivasc_mask),...
+%             2^14.*uint16(single_vessel_mask));
+%         imshow(k); 
+%         pause(0.1);
 %       %%
     end
 end
