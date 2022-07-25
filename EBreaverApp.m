39708d83d458adb7cfd6dae8299b4dcffe77f480
sharonkatz510 <sharonkatz510@gmail.com>
2022-07-25 16:04:15 +0300

    Changes to the readme file

---
+++ EBreaverApp.m
@@ -0,0 +1,221 @@
+classdef EBreaverApp < matlab.apps.AppBase
+
+    % Properties that correspond to app components
+    properties (Access = public)
+        EBreaverUIFigure                matlab.ui.Figure
+        StartDistLabel                  matlab.ui.control.Label
+        startDistSpinner                matlab.ui.control.Spinner
+        ExporttoExcelButton             matlab.ui.control.Button
+        NormalizeredCheckBox            matlab.ui.control.CheckBox
+        CreateanalysisobjectButton      matlab.ui.control.Button
+        OpenREAVERGUIButton             matlab.ui.control.Button
+        BrowseTestButton                matlab.ui.control.Button
+        BrowseControlButton             matlab.ui.control.Button
+        ProcessfoldersButton            matlab.ui.control.Button
+        perivascularwidthpxSpinner      matlab.ui.control.Spinner
+        perivascularwidthpxSpinnerLabel  matlab.ui.control.Label
+        TestdirectoryEditField          matlab.ui.control.EditField
+        TestdirectoryEditFieldLabel     matlab.ui.control.Label
+        ControldirectoryEditField       matlab.ui.control.EditField
+        ControldirectoryEditFieldLabel  matlab.ui.control.Label
+    end
+
+    % Callbacks that handle component events
+    methods (Access = private)
+
+        % Code that executes after component creation
+        function startupFcn(app)
+            try
+                evalin("base",'depndenciesImported')
+            catch 
+                DEV_INITIALIZE;
+                assignin("base",'depndenciesImported',true)
+            end
+            
+        end
+
+        % Button pushed function: ProcessfoldersButton
+        function ProcessfoldersButtonPushed(app, event)
+            control_dir = app.ControldirectoryEditField.Value;
+            test_dir = app.TestdirectoryEditField.Value;
+            n_px = app.perivascularwidthpxSpinner.Value;
+            diffu = app.startDistSpinner.Value;
+            normalize_red = app.NormalizeredCheckBox.Value;
+            if ~isempty(control_dir)
+                disp('Starting to work on control files')
+                EB_analysis_entire_folder(n_px,control_dir,normalize_red,diffu);
+                disp('Finished processing control files')
+            end
+            if ~isempty(test_dir)
+                disp('Starting to work on test files')
+                EB_analysis_entire_folder(n_px,test_dir,normalize_red,diffu); 
+                disp('Finished processing test files')
+            end
+        end
+
+        % Button pushed function: BrowseControlButton
+        function BrowseControlButtonPushed(app, event)
+            control_dir = uigetdir(pwd,'Choose control directory');
+            app.ControldirectoryEditField.Value = control_dir;
+            figure(app.EBreaverUIFigure)
+        end
+
+        % Button pushed function: BrowseTestButton
+        function BrowseTestButtonPushed(app, event)
+            test_dir = uigetdir(pwd,'Choose test directory');
+            app.TestdirectoryEditField.Value = test_dir;
+            figure(app.EBreaverUIFigure)
+        end
+
+        % Button pushed function: OpenREAVERGUIButton
+        function OpenREAVERGUIButtonPushed(app, event)
+            REAVER_GUI;
+        end
+
+        % Button pushed function: CreateanalysisobjectButton
+        function CreateanalysisobjectButtonPushed(app, event)
+            results = EB_analysis;
+            assignin("base","results",results);
+        end
+
+        % Button pushed function: ExporttoExcelButton
+        function ExporttoExcelButtonPushed(app, event)
+            [control_filename,control_path] = ...
+                uiputfile({'.csv','.xlsx'},...
+                'Save control file','Control.csv');
+            [test_filename,test_path] = ...
+                uiputfile({'.csv','.xlsx'},...
+                'Save test file','Test.csv');
+            results = evalin('base','results');
+            ths = 2:10;
+            results.writecsv(ths,fullfile(control_path,control_filename),...
+                fullfile(test_path,test_filename));
+        end
+    end
+
+    % Component initialization
+    methods (Access = private)
+
+        % Create UIFigure and components
+        function createComponents(app)
+
+            % Create EBreaverUIFigure and hide until all components are created
+            app.EBreaverUIFigure = uifigure('Visible', 'off');
+            app.EBreaverUIFigure.Color = [0.9412 0.9412 0.9412];
+            app.EBreaverUIFigure.Position = [100 100 535 368];
+            app.EBreaverUIFigure.Name = 'EB reaver';
+
+            % Create ControldirectoryEditFieldLabel
+            app.ControldirectoryEditFieldLabel = uilabel(app.EBreaverUIFigure);
+            app.ControldirectoryEditFieldLabel.HorizontalAlignment = 'right';
+            app.ControldirectoryEditFieldLabel.Position = [70 254 94 22];
+            app.ControldirectoryEditFieldLabel.Text = 'Control directory';
+
+            % Create ControldirectoryEditField
+            app.ControldirectoryEditField = uieditfield(app.EBreaverUIFigure, 'text');
+            app.ControldirectoryEditField.Position = [179 253 204 23];
+
+            % Create TestdirectoryEditFieldLabel
+            app.TestdirectoryEditFieldLabel = uilabel(app.EBreaverUIFigure);
+            app.TestdirectoryEditFieldLabel.HorizontalAlignment = 'right';
+            app.TestdirectoryEditFieldLabel.Position = [70 212 77 22];
+            app.TestdirectoryEditFieldLabel.Text = 'Test directory';
+
+            % Create TestdirectoryEditField
+            app.TestdirectoryEditField = uieditfield(app.EBreaverUIFigure, 'text');
+            app.TestdirectoryEditField.Position = [179 211 204 23];
+
+            % Create perivascularwidthpxSpinnerLabel
+            app.perivascularwidthpxSpinnerLabel = uilabel(app.EBreaverUIFigure);
+            app.perivascularwidthpxSpinnerLabel.HorizontalAlignment = 'right';
+            app.perivascularwidthpxSpinnerLabel.Position = [70 160 124 22];
+            app.perivascularwidthpxSpinnerLabel.Text = 'perivascular width [px]';
+
+            % Create perivascularwidthpxSpinner
+            app.perivascularwidthpxSpinner = uispinner(app.EBreaverUIFigure);
+            app.perivascularwidthpxSpinner.Position = [209 155 56 31];
+
+            % Create ProcessfoldersButton
+            app.ProcessfoldersButton = uibutton(app.EBreaverUIFigure, 'push');
+            app.ProcessfoldersButton.ButtonPushedFcn = createCallbackFcn(app, @ProcessfoldersButtonPushed, true);
+            app.ProcessfoldersButton.Position = [283 137 122 30];
+            app.ProcessfoldersButton.Text = 'Process folders';
+
+            % Create BrowseControlButton
+            app.BrowseControlButton = uibutton(app.EBreaverUIFigure, 'push');
+            app.BrowseControlButton.ButtonPushedFcn = createCallbackFcn(app, @BrowseControlButtonPushed, true);
+            app.BrowseControlButton.Position = [404 253 72 22];
+            app.BrowseControlButton.Text = 'Browse';
+
+            % Create BrowseTestButton
+            app.BrowseTestButton = uibutton(app.EBreaverUIFigure, 'push');
+            app.BrowseTestButton.ButtonPushedFcn = createCallbackFcn(app, @BrowseTestButtonPushed, true);
+            app.BrowseTestButton.Position = [404 212 72 22];
+            app.BrowseTestButton.Text = 'Browse';
+
+            % Create OpenREAVERGUIButton
+            app.OpenREAVERGUIButton = uibutton(app.EBreaverUIFigure, 'push');
+            app.OpenREAVERGUIButton.ButtonPushedFcn = createCallbackFcn(app, @OpenREAVERGUIButtonPushed, true);
+            app.OpenREAVERGUIButton.Position = [173 306 215 34];
+            app.OpenREAVERGUIButton.Text = 'Open REAVER GUI for segmentation';
+
+            % Create CreateanalysisobjectButton
+            app.CreateanalysisobjectButton = uibutton(app.EBreaverUIFigure, 'push');
+            app.CreateanalysisobjectButton.ButtonPushedFcn = createCallbackFcn(app, @CreateanalysisobjectButtonPushed, true);
+            app.CreateanalysisobjectButton.Position = [181 60 208 25];
+            app.CreateanalysisobjectButton.Text = 'Create analysis object';
+
+            % Create NormalizeredCheckBox
+            app.NormalizeredCheckBox = uicheckbox(app.EBreaverUIFigure);
+            app.NormalizeredCheckBox.Text = 'Normalize red';
+            app.NormalizeredCheckBox.Position = [421 139 97 25];
+
+            % Create ExporttoExcelButton
+            app.ExporttoExcelButton = uibutton(app.EBreaverUIFigure, 'push');
+            app.ExporttoExcelButton.ButtonPushedFcn = createCallbackFcn(app, @ExporttoExcelButtonPushed, true);
+            app.ExporttoExcelButton.Position = [221 21 121 27];
+            app.ExporttoExcelButton.Text = 'Export to Excel';
+
+            % Create startDistSpinner
+            app.startDistSpinner = uispinner(app.EBreaverUIFigure);
+            app.startDistSpinner.Position = [209 117 57 31];
+
+            % Create StartDistLabel
+            app.StartDistLabel = uilabel(app.EBreaverUIFigure);
+            app.StartDistLabel.HorizontalAlignment = 'right';
+            app.StartDistLabel.Position = [31 121 164 22];
+            app.StartDistLabel.Text = 'Distance from vessel wall [px]';
+
+            % Show the figure after all components are created
+            app.EBreaverUIFigure.Visible = 'on';
+        end
+    end
+
+    % App creation and deletion
+    methods (Access = public)
+
+        % Construct app
+        function app = EBreaverApp
+
+            % Create UIFigure and components
+            createComponents(app)
+
+            % Register the app with App Designer
+            registerApp(app, app.EBreaverUIFigure)
+
+            % Execute the startup function
+            runStartupFcn(app, @startupFcn)
+
+            if nargout == 0
+                clear app
+            end
+        end
+
+        % Code that executes before app deletion
+        function delete(app)
+
+            % Delete UIFigure when app is deleted
+            delete(app.EBreaverUIFigure)
+        end
+    end
+end
--- README.md
+++ README.md
@@ -4,31 +4,39 @@ Rapid Editable Analysis of Vessel Elements Routine, utilized for quantification 
 
 ## Dependencies:
 * Tested on MATLAB 2021a, requires image processing toolbox.
+* Before starting the process make sure the control and test tiff files are placed in seperate folders
 
 ## Initialization
-1. Open the extracted folder and open the app EB_reaver (double click)
-	-The following window will pop-up:
+1. Open the extracted folder in matlab and run the main app by executing:
+	>EBreaverApp
+	in the MATLAB command terminal
+The following window will pop-up:
 ![screenshot](resources/guiUI.png)
 
 ## Vessel segmentation
-1. type *"REAVER_GUI"* on the MATLAB terminal and press "enter"
-	- This will open the REAVER application
-2. Follow the REAVER pipeline described in: https://github.com/uva-peirce-cottler-lab/public_REAVER
-	- The app works only with 8-bit images 
-	(for batch conversion of 16-bit to 8 bit use the imageJ macro "batch convert 8bit.ijm")
+1. Open the REAVER GUI via the dedicated push button
+2. Follow the REAVER segmentation pipeline described in: https://github.com/uva-peirce-cottler-lab/public_REAVER
 	- Use the FITC-dextran (green) channl for segmentation
+	- This will create a ".mat" file for each segmented tiff file
+	- It will also create a "UserVerified.mat" file that points to verified files for future analysis
 
 ## Batch analysis of segmented images
-1. type *EB_analysis_entire_folder(n_px)*, replace n_px with the desired perivascular are width in pixels.
-    - The function will prompt you to choose the segmented images folder.
-2. run the same script on the control and test data.
+1. Choose control file directory and test file directory.
+	These should contain the already segmented tiff files
+2. Specify the perivascular width (d) and the distance from vessel wall (w_i) to fit your experiment:
+![screenshot](resources/perivascularArea.png)
+3. Check the "normalize red" checkbox to perform frame-level normalization of the red channel to a range of [0,1]
+4. Press the process folders push button
+	- This will create an "EB_analysis_<hyper_parameters>.mat" file in each of the directories
+	- The created file name indicated the analysis hyper-parameters
 
-## Comparison of control and test groups
-1. create an "EB_analysis" object using the following syntax:
-	> object = EB_analysis.
-	- This will prompt you to load the EB_analysis_npx.m files of the control and test groups
+## Create a summary object
+1. Press the "Create analysis object" push button
+2. Choose the approperiate EB_analysis file from the control directory
+3. Choose the appropriate EB_analysis file from the test directory
+	- This will generate a summary object of class "EB_analysis" and save it to the open matlab workspace as "results"
 
-The EB_analysis object has the following plotting methods:
+The EB_analysis class object has the following plotting methods:
 
 **scatterPlot**
 Creates a scatter plot where each point represents a vessel segment and the axis are:
